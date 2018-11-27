5f7dc75371421c02510c90393f376933244ed2bc
hanyang <shuerhy@163.com>
2018-11-28 00:04:44 +0800

    left stake redeem method

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,134 +1,166 @@
 <template>
-<div>
-  <div class="header">
-    <img src="../assets/eosdice.png" class="logo"/>
-    <div class="right">
-      <div class="mobile-none">
+  <div>
+    <div class="header">
+      <img src="../assets/eosdice.png" class="logo">
+      <div class="right">
+        <div class="mobile-none">
+          <el-button type="text" @click="(window.open('https://jacks.eosdice.vip'))">EOSJACKS</el-button>
+          <el-button type="text">历史公告</el-button>
+          <el-button
+            type="text"
+            @click="window.open('https://deltadex.io/embed/eosdicevip/eosbocai1111-BOCAI')"
+          >交易BOCAI</el-button>
+          <el-button type="text" @click="openVip">VIP</el-button>
+          <el-button type="text" @click="openRoadMap">Road Map</el-button>
+          <el-button type="text" @click="divideDialog = true">分红池</el-button>
+          <el-button type="text" @click="openToken">TOKEN介绍</el-button>
+          <el-button type="text" @click="openHowto">玩法介绍</el-button>
+          <el-button type="text" @click="inviteFriend=true">邀请好友</el-button>
+          <el-button type="text" style="margin: 0 10px;" @click="openContact">联系我们</el-button>
+        </div>
+        <el-button type="text">
+          <span v-if="username" @click="logout">
+            {{username}}
+            <i class="el-icon-caret-bottom"></i>
+          </span>
+          <span v-else @click="login">登录</span>
+        </el-button>
+        <el-dialog :visible.sync="inviteFriend" custom-class="custom-class">
+          <h3 slot="title">通过邀请获得返利!</h3>
+          <div class="refer-class">
+            <input v-model="myRefer" id="referId">
+            <el-button type="primary" @click="copyLink">复制</el-button>
+          </div>
+          <h3 style="color:#fff; text-align:left; margin-top:10px">请勿错过！邀请好友即可获得他们 0.151% 的投注额</h3>
+        </el-dialog>
+        <el-dropdown @command="changeLang" class="dice-dropdwon">
+          <span class="el-dropdown-link">
+            <div v-if="dicelang === 'en'">
+              <img src="../assets/lang-en.svg">
+              &nbsp;{{$t("header.en")}}
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </div>
+            <div v-if="dicelang === 'cn'">
+              <img src="../assets/lang-cn.svg">
+              &nbsp;{{$t("header.cn")}}
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </div>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="en">
+              <img src="../assets/lang-en.svg">
+              &nbsp;{{$t("header.en")}}
+            </el-dropdown-item>
+            <el-dropdown-item command="cn">
+              <img src="../assets/lang-cn.svg">
+              &nbsp;{{$t("header.cn")}}
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <i class="el-icon-menu dice-menu" @click="mobileShow = !mobileShow"></i>
+      </div>
+    </div>
+    <transition name="el-zoom-in-center">
+      <div v-show="mobileShow">
         <el-button type="text" @click="(window.open('https://jacks.eosdice.vip'))">EOSJACKS</el-button>
         <el-button type="text">历史公告</el-button>
-        <el-button type="text" @click="window.open('https://deltadex.io/embed/eosdicevip/eosbocai1111-BOCAI')">交易BOCAI</el-button>
+        <el-button
+          type="text"
+          @click="window.open('https://deltadex.io/embed/eosdicevip/eosbocai1111-BOCAI')"
+        >交易BOCAI</el-button>
         <el-button type="text" @click="openVip">VIP</el-button>
         <el-button type="text" @click="openRoadMap">Road Map</el-button>
         <el-button type="text" @click="divideDialog = true">分红池</el-button>
         <el-button type="text" @click="openToken">TOKEN介绍</el-button>
         <el-button type="text" @click="openHowto">玩法介绍</el-button>
         <el-button type="text" @click="inviteFriend=true">邀请好友</el-button>
-        <el-button type="text" style="margin: 0 10px;" @click="openContact">联系我们</el-button>
+        <el-button type="text" style="margin-left:10px;" @click="openContact">联系我们</el-button>
       </div>
-      <el-button type="text">
-        <span v-if="username" @click="logout">{{username}}<i class="el-icon-caret-bottom"></i></span>
-        <span v-else @click="login">登录</span>
-      </el-button>
-      <el-dialog :visible.sync="inviteFriend" custom-class="custom-class">
-        <h3 slot="title">通过邀请获得返利!</h3>
-        <div class="refer-class">
-          <input v-model="myRefer" id="referId"/>
-          <el-button type="primary" @click="copyLink">复制</el-button>
+    </transition>
+    <!-- dialog -->
+    <!-- token 开始-->
+    <el-dialog custom-class="custom-class" :visible.sync="tokenDialog">
+      <div v-html="$t('header.token')"></div>
+    </el-dialog>
+    <!-- token 结束 -->
+    <!-- 分红池 开始 -->
+    <el-dialog custom-class="divide-class" :visible.sync="divideDialog">
+      <div class="title">
+        <div>
+          <h2 :class="{'on-tab': tab1}" @click="tab1=true">分红池</h2>
         </div>
-        <h3 style="color:#fff; text-align:left; margin-top:10px">请勿错过！邀请好友即可获得他们 0.151% 的投注额</h3>
-      </el-dialog>
-      <el-dropdown @command="changeLang" class="dice-dropdwon">
-        <span class="el-dropdown-link">
-          <div v-if="dicelang === 'en'"><img src="../assets/lang-en.svg" /> &nbsp;{{$t("header.en")}}<i class="el-icon-arrow-down el-icon--right"></i></div>
-          <div v-if="dicelang === 'cn'"><img src="../assets/lang-cn.svg" /> &nbsp;{{$t("header.cn")}}<i class="el-icon-arrow-down el-icon--right"></i></div>
-        </span>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item command="en"><img src="../assets/lang-en.svg" /> &nbsp;{{$t("header.en")}}</el-dropdown-item>
-          <el-dropdown-item command="cn"><img src="../assets/lang-cn.svg" /> &nbsp;{{$t("header.cn")}}</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <i class="el-icon-menu dice-menu" @click="mobileShow = !mobileShow"></i>
-    </div>
+        <div>
+          <h2 :class="{'on-tab': !tab1}" @click="tab1=false">质押与分红</h2>
+        </div>
+      </div>
+      <div class="divide" v-show="tab1">
+        <h3>质押与分红</h3>
+        <div class="item">
+          <div class="left">
+            <img src="../assets/eos.png">
+            <p>历史总分红: {{totalShare}} EOS</p>
+          </div>
+          <div class="right">
+            <div>
+              <p>你一共领取了</p>
+              <p>{{totalout}} EOS</p>
+            </div>
+            <div>
+              <p>可以领取分红</p>
+              <p> {{canGet}} EOS</p>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="item">
+          <div class="left">
+            <img src="../assets/token.png">
+            <p>全网质押比:{{stakeRatio}}%</p>
+          </div>
+          <div class="right">
+            <div>
+              <p>当前流通</p>
+              <p>{{allCirculate}} BOCAI</p>
+            </div>
+            <div>
+              <p>当前质押</p>
+              <p>{{totalStaked}} BOCAI</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="stake" v-show="!tab1">
+        <div class="my-divide">
+          <div class="item-1">
+            <div class="title">我的分红: {{canGet}} EOS</div>
+            <el-button type="primary">提现</el-button>
+          </div>
+        </div>
+        <div class="stake-content">
+          <div class="title">可质押: {{myBocai}} BOCAI</div>
+          <div class="input-content">
+            <el-input :placeholder="mostStake" class="dice-input" v-model="stake"></el-input>
+            <el-button type="primary">质押</el-button>
+          </div>
+        </div>
+        <div class="stake-content">
+          <div class="title">已质押: {{Number(currentStake).toFixed(4)}} BOCAI(占质押总量的 0.00 %)</div>
+          <div class="input-content">
+            <el-input :placeholder="mostRedeem" class="dice-input" v-model="redeem"></el-input>
+            <el-button type="primary">赎回</el-button>
+          </div>
+          <div class="tips">赎回需24小时，赎回中的BOCAI不享受分红</div>
+        </div>
+      </div>
+    </el-dialog>
+    <!-- 分红池 结束 -->
+    <!-- dialog -->
   </div>
-  <transition name="el-zoom-in-center">
-    <div v-show="mobileShow">
-      <el-button type="text" @click="(window.open('https://jacks.eosdice.vip'))">EOSJACKS</el-button>
-      <el-button type="text">历史公告</el-button>
-      <el-button type="text" @click="window.open('https://deltadex.io/embed/eosdicevip/eosbocai1111-BOCAI')">交易BOCAI</el-button>
-      <el-button type="text" @click="openVip">VIP</el-button>
-      <el-button type="text" @click="openRoadMap">Road Map</el-button>
-      <el-button type="text" @click="divideDialog = true">分红池</el-button>
-      <el-button type="text" @click="openToken">TOKEN介绍</el-button>
-      <el-button type="text" @click="openHowto">玩法介绍</el-button>
-      <el-button type="text" @click="inviteFriend=true">邀请好友</el-button>
-      <el-button type="text" style="margin-left:10px;" @click="openContact">联系我们</el-button>
-    </div>
-  </transition>
-  <!-- dialog -->
-
-  <!-- token 开始-->
-  <el-dialog custom-class="custom-class" :visible.sync="tokenDialog">
-    <div v-html="$t('header.token')"></div>
-  </el-dialog>
-  <!-- token 结束 -->
-
-  <!-- 分红池 开始 -->
-  <el-dialog custom-class="divide-class" :visible.sync="divideDialog">
-    <div class="title">
-      <div><h2 :class="{'on-tab': tab1}" @click="tab1=true">分红池</h2></div>
-      <div><h2 :class="{'on-tab': !tab1}" @click="tab1=false">质押与分红</h2></div>
-    </div>
-    <div class="divide" v-show="tab1">
-      <h3>质押与分红</h3>
-      <div class="item">
-        <div class="left">
-          <img src="../assets/eos.png" />
-          <p>历史总分红:</p>
-        </div>
-        <div class="right">
-          <div>
-            <p>你一共领取了</p>
-            <p>0.0000 EOS</p>
-          </div>
-          <div>
-            <p>可以领取分红</p>
-            <p>0.0000 EOS</p>
-          </div>
-        </div>
-      </div>
-      <br />
-      <div class="item">
-        <div class="left">
-          <img src="../assets/token.png" />
-          <p>全网质押比:</p>
-        </div>
-        <div class="right">
-          <div>
-            <p>当前流通</p>
-            <p>0.0000 EOS</p>
-          </div>
-          <div>
-            <p>当前质押</p>
-            <p>0.0000 EOS</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="stake" v-show="!tab1">
-      <div class="my-divide">
-        <div class="item-1">
-          <div class="title">我的分红: 0.0000 EOS</div><el-button type="primary">提现</el-button>
-        </div>
-      </div>
-      <div class="stake-content">
-        <div class="title">可质押: 0 BOCAI</div>
-        <div class="input-content"><el-input v-model="input" placeholder="请输入内容" class="dice-input"></el-input><el-button type="primary">质押</el-button></div>
-      </div>
-      <div class="stake-content">
-        <div class="title">已质押: 0.0000 BOCAI(占质押总量的 0.00 %)</div>
-        <div class="input-content"><el-input v-model="input" placeholder="请输入内容" class="dice-input"></el-input><el-button type="primary">赎回</el-button></div>
-        <div class="tips">赎回需24小时，赎回中的BOCAI不享受分红</div>
-      </div>
-    </div>
-  </el-dialog>
-  <!-- 分红池 结束 -->
-
-  <!-- dialog -->
-</div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { restApi } from "@/network/transtion";
 import { login } from "@/util/login";
 export default {
   name: "diceheader",
@@ -142,8 +174,67 @@ export default {
       tokenDialog: false,
       divideDialog: false,
       // 是否在分红池选项
-      tab1: true
+      tab1: true,
+      // 历史总分红
+      totalShare: '0.0000',
+      // 当前质押
+      totalStaked: '0.0000',
+      // 每个股份的价格
+      earningsPerShare: '0.0000',
+      // 已领取分红
+      totalout: '0.0000',
+      // 已领取分红
+      payout: '0.0000',
+      // 质押的BOCAI
+      currentStake: '0.0000',
+      // 当前流通
+      allCirculate: '0.0000',
+      // 需要减去的流通的BOCAI
+      subBocai: 0,
+      supply: 8800000000,
+      myBocai: '0.0000',
+      // 质押
+      stake: '',
+      redeem: ''
     };
+  },
+  watch: {
+    divideDialog(newValue) {
+      if (newValue) {
+        restApi.getTableRows({
+          code: "eosbocaidivi",
+          scope: "eosbocaidivi",
+          table: "global",
+          json: true
+        }).then(res => {
+          // console.log(res, 'poll')
+          if (res.rows && res.rows.length !== 0) {
+            this.totalStaked = res.rows[0].total_staked / 10000;
+            this.totalShare = res.rows[0].total_share / 10000;
+            this.earningsPerShare = parseInt(res.rows[0].earnings_per_share.substr(2).match(/.{1,2}/g).reverse().join(""), 16) || 0;
+          }
+        });
+        if (this.username) {
+          restApi.getTableRows({
+            code: "eosbocaidivi",
+            scope: this.username,
+            table: "voters",
+            json: true
+          }).then(res => {
+            console.log(res, 'voters')
+            if (res.rows && res.rows.length !== 0) {
+              this.currentStake = res.rows[0].staked / 10000;
+              this.payout = res.rows[0].payout;
+              this.totalout = (res.rows[0].totalout / 10000).toFixed(4);
+            }
+          })
+        }
+        if (this.subBocai) {
+          this.allCirculate = (this.supply - this.subBocai).toFixed(4);
+        }
+        this.bocaiAmount();
+      }
+    }
   },
   methods: {
     changeLang(command) {
@@ -215,13 +306,129 @@ export default {
         lockScroll: false,
         customClass: "custom-class"
       })
+    },
+    // 需要減去的BOCAI
+    getSubCirculate() {
+      const _this = this;
+      Promise.all([
+        restApi.getTableRows({
+          json: true,
+          code: "eosbocai1111",
+          table: "accounts",
+          scope: "eosbocaicomm"
+        }),
+        restApi.getTableRows({
+          json: true,
+          code: "eosbocai1111",
+          table: "accounts",
+          scope: "eosbocaidevv"
+        }),
+        restApi.getTableRows({
+          json: true,
+          code: "eosbocai1111",
+          table: "accounts",
+          scope: "eosbocai2222"
+        }),
+        restApi.getTableRows({
+          json: true,
+          code: "eosbocai1111",
+          table: "accounts",
+          scope: "jk2uslllkjfd"
+        }),
+        restApi.getTableRows({
+          json: true,
+          code: "eosbocai1111",
+          table: "accounts",
+          scope: "bocaiairgrab"
+        })
+      ]).then(
+        ([
+          commBalance,
+          devBalance,
+          bocai2222Balance,
+          jk2uslllkjfdBalance,
+          bocaiairgrabBalance
+        ]) => {
+          let _commBalance = 0;
+          let _devBalance = 0;
+          let _bocai2222Balance = 0;
+          let _jk2uslllkjfdBalance = 0;
+          let _bocaiairgrabBalance = 0;
+          if (commBalance.rows.length) {
+            _commBalance = commBalance.rows[0].balance.split(" ")[0];
+          }
+          if (devBalance.rows.length) {
+            _devBalance = devBalance.rows[0].balance.split(" ")[0];
+          }
+          if (bocai2222Balance.rows.length) {
+            _bocai2222Balance = bocai2222Balance.rows[0].balance.split(" ")[0];
+          }
+          if (bocaiairgrabBalance.rows.length) {
+            _bocaiairgrabBalance = bocaiairgrabBalance.rows[0].balance.split(
+              " "
+            )[0];
+          }
+          if (jk2uslllkjfdBalance.rows.length) {
+            _jk2uslllkjfdBalance = jk2uslllkjfdBalance.rows[0].balance.split(
+              " "
+            )[0];
+          }
+          _this.subBocai = (
+            Number(_commBalance) +
+            Number(_devBalance) +
+            Number(_bocai2222Balance) +
+            Number(_jk2uslllkjfdBalance) +
+            Number(_bocaiairgrabBalance));
+        }
+      );
+    },
+    bocaiAmount() {
+      if (this.username) {
+        restApi.getTableRows({
+          code: 'eosbocai1111',
+          table: 'accounts',
+          scope: this.username,
+          json: true
+        }).then((res) => {
+          this.myBocai = res.rows[0].balance.split(" ")[0];
+        });
+      }
     }
   },
   computed: {
     ...mapGetters(["username"]),
     myRefer() {
       return "https://eosdice.vip?ref=" + (this.username ? this.username : "");
+    },
+    canGet() {
+      if (this.earningsPerShare && this.currentStake) {
+        return (((this.earningsPerShare * this.currentStake * 10000) / Math.pow(2, 32) - this.payout) / 10000).toFixed(4);
+      } else {
+        return '0.0000';
+      }
+    },
+    stakeRatio() {
+      return ((this.totalStaked / this.allCirculate) * 100).toFixed(2);
+    },
+    mostStake() {
+      return `最多可质押${this.myBocai}BOCAI`;
+    },
+    mostRedeem() {
+      return `最多可赎回${Number(this.currentStake).toFixed(4)}BOCAI`;
     }
+  },
+  mounted() {
+    this.getSubCirculate();
+    restApi.getTableRows({
+      json: true,
+      code: "eosbocai1111",
+      table: "stat",
+      scope: "BOCAI"
+    }).then(res => {
+      if(res.rows && res.rows.length !== 0) {
+        this.supply = res.rows[0].supply.slice(0, -6);
+      }
+    });
   }
 };
 </script>
