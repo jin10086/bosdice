9b4cdf7d28b53f2fda544c440b593c350b00f2f0
jin10086 <1019855680@qq.com>
2018-12-09 20:49:32 +0800

    Update transtion.js

--- src/network/transtion.js
+++ src/network/transtion.js
@@ -3,7 +3,7 @@ import Eos from "eosjs";
 
 export const network = {
   blockchain: "eos",
-  host: "api.eosbeijing.one",
+  host: "geo.eosasia.one",
   port: 443,
   protocol: "https",
   chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
