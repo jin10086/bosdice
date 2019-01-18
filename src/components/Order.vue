d326911c9c6fd4786be55fe5e0d78b5e2b20d7a4
jin10086 <1019855680@qq.com>
2019-01-18 22:16:50 +0800

    api.

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -19,7 +19,8 @@
             </tr>
           </thead>
           <tbody>
-            <tr :class="{win: bet.random_roll < bet.roll_under, lose: !(bet.random_roll < bet.roll_under)}" v-for="(bet, index) in allBets" :key="index">
+            <tr :class="{win: bet.random_roll < bet.roll_under, lose: !(bet.random_roll < bet.roll_under)}" v-for="(bet, index) in allBets"
+              :key="index">
               <td>{{bet.block_time}}</td>
               <td>{{bet.player}}</td>
               <td>{{bet.roll_under}}</td>
@@ -43,7 +44,8 @@
             </tr>
           </thead>
           <tbody>
-            <tr :class="{win: bet.random_roll < bet.roll_under, lose: !(bet.random_roll < bet.roll_under)}" v-for="(bet, index) in myBets" :key="index">
+            <tr :class="{win: bet.random_roll < bet.roll_under, lose: !(bet.random_roll < bet.roll_under)}" v-for="(bet, index) in myBets"
+              :key="index">
               <td>{{bet.block_time}}</td>
               <td>{{bet.player}}</td>
               <td>{{bet.roll_under}}</td>
@@ -62,188 +64,215 @@
 </template>
 
 <script>
-import { handleData } from "@/network/ws.js";
-import { mapGetters } from "vuex";
-export default {
-  name: "dice-order",
-  data() {
-    return {
-      tab1: true,
-      tab2: false,
-      tab3: false,
-      allBets: [],
-      myBets: [],
-      ws_identify: ""
-    };
-  },
-  methods: {
-    changeTab(type) {
-      switch (type) {
-        case 1:
-          this.tab1 = true;
-          this.tab2 = this.tab3 = false;
-          break;
-        case 2:
-          this.tab2 = true;
-          this.tab1 = this.tab3 = false;
-          break;
-        case 3:
-          this.tab3 = true;
-          this.tab1 = this.tab2 = false;
-      }
-    }
-  },
-  props: ["activeToken"],
-  watch: {
-    allBets(newValue) {
-      if (this.username) {
-        this.myBets = newValue.filter(item => {
-          return item.player === this.username;
-        })
-        if (this.myBets.length === 31) {
-          this.myBets.shift();
+  import {
+    restApi
+  } from "@/network/transtion.js";
+  import {
+    mapGetters
+  } from "vuex";
+  export default {
+    name: "dice-order",
+    data() {
+      return {
+        tab1: true,
+        tab2: false,
+        tab3: false,
+        global_action_seq: 0,
+        allBets: [],
+        myBets: [],
+        ws_identify: ""
+      };
+    },
+    methods: {
+      changeTab(type) {
+        switch (type) {
+          case 1:
+            this.tab1 = true;
+            this.tab2 = this.tab3 = false;
+            break;
+          case 2:
+            this.tab2 = true;
+            this.tab1 = this.tab3 = false;
+            break;
+          case 3:
+            this.tab3 = true;
+            this.tab1 = this.tab2 = false;
         }
       }
-    }
-  },
-
-  computed: {
-    ...mapGetters(["username"])
-  },
-
-  mounted() {
-    const _this = this;
-    this.ws_identify = this.$ws.getActionTraces(
-      {
-        account: "eosbocailogs",
-        action_name: "result",
-        receiver: "eosbocailogs"
-      },
-      { req_id: "roll_result" }
-    );
-    this.ws_identify.onMessage(message => {
-      if(handleData(message)) _this.allBets.unshift(handleData(message));
-      const mes = handleData(message);
-      if (this.username && mes) {
-        if (mes.player === this.username) {
-          this.$store.dispatch("UPDATE_EOS_ASYNC");
-          this.$store.dispatch("UPDATE_TOKEN_ASYNC", {
-            type: this.activeToken === "eos" ? "bocai" : this.activeToken
-          });
-          if(mes.random_roll < mes.roll_under) {
-            this.$message.success(this.$t("order.win", {
-              random: mes.random_roll,
-              payout: mes.payout
-            }));
-          } else {
-            this.$message.error(this.$t("order.lose", {
-              random: mes.random_roll,
-              amount: mes.amount
-            }));
+    },
+    props: ["activeToken"],
+    watch: {
+      allBets(newValue) {
+        if (this.username) {
+          this.myBets = newValue.filter(item => {
+            return item.player === this.username;
+          })
+          if (this.myBets.length === 31) {
+            this.myBets.shift();
           }
         }
       }
-      if(_this.allBets.length > 30) {
-        _this.allBets.shift();
-      }
-    });
-  },
+    },
 
-  destroyed() {
-    this.ws_identify.unlisten();
-  }
-};
+    computed: {
+      ...mapGetters(["username"])
+    },
+
+    mounted() {
+      const _this = this;
+      function getresult() {
+        restApi.getActions("bosdicelogss", -1, -30).then(res => {
+          if (_this.global_action_seq == 0) {
+            _this.global_action_seq = res.actions[res.actions.length - 1].global_action_seq;
+          } else {
+            res.actions.forEach(element => {
+              if (element.global_action_seq > _this.global_action_seq) {
+                if (element.action_trace.act.name == 'result') {
+                  _this.global_action_seq = element.global_action_seq;
+                  let mes = element.action_trace.act.data.result;
+                  mes.block_time = element.block_time;
+                  _this.allBets.unshift(mes);
+                  if (mes.player === this._username) {
+                    this.$store.dispatch("UPDATE_EOS_ASYNC");
+                    this.$store.dispatch("UPDATE_TOKEN_ASYNC", {
+                      type: this.activeToken === "eos" ? "bocai" : this.activeToken
+                    });
+                    if (mes.random_roll < mes.roll_under) {
+                      console.log("win");
+                      _this.$message.success(this.$t("order.win", {
+                        random: mes.random_roll,
+                        payout: mes.payout
+                      }));
+                    } else {
+                      console.log("lose");
+                      _this.$message.error(this.$t("order.lose", {
+                        random: mes.random_roll,
+                        amount: mes.amount
+                      }));
+                    }
+                  }
+
+                  // if (this.allBets.length > 30) {
+                  //   this.allBets.shift();
+                  // }
+                }
+              }
+            });
+          }
+        })
+      }
+      setInterval(getresult,10000);
+
+    },
+
+  };
 </script>
 
 <style lang="less">
-@media (max-width: 768px) {
-  .order {
-    width: 100% !important;
-  }
-}
-
-.order {
-  background-color: #161933;
-  padding: 30px 0 50px;
   @media (max-width: 768px) {
-    .order-container {
+    .order {
       width: 100% !important;
     }
   }
-  .order-container {
-    width: 1090px;
-    min-height: 200px;
-    margin: 0 auto;
+
+  .order {
+    background-color: #161933;
+    padding: 30px 0 50px;
+
     @media (max-width: 768px) {
-      .order-tab {
-        height: 40px !important;
-        span {
-          height: 40px !important;
-          line-height: 40px !important;
-        }
+      .order-container {
+        width: 100% !important;
       }
     }
-    .order-tab {
-      width: 100%;
-      height: 60px;
-      padding: 0 20px;
-      background-color: #1c233f;
-      text-align: left;
-      color: #989a99;
+
+    .order-container {
+      width: 1090px;
+      min-height: 200px;
+      margin: 0 auto;
+
       @media (max-width: 768px) {
-        span {
-          padding: 0 10px !important;
+        .order-tab {
+          height: 40px !important;
+
+          span {
+            height: 40px !important;
+            line-height: 40px !important;
+          }
         }
       }
-      span {
-        cursor: pointer;
-        padding: 0 30px;
-        display: inline-block;
-        font-size: 12px;
+
+      .order-tab {
+        width: 100%;
         height: 60px;
-        line-height: 60px;
-      }
-      .active-tab {
-        color: #fff;
-        border-bottom: 1px solid #fff;
-      }
-    }
-    .order-table {
-      border-collapse: collapse;
-    }
-    table {
-      vertical-align: center;
-      tr {
-        border-top: 1px dashed #1e1e1e;
-        background: #1c233f;
-        th,
-        td {
+        padding: 0 20px;
+        background-color: #1c233f;
+        text-align: left;
+        color: #989a99;
+
+        @media (max-width: 768px) {
+          span {
+            padding: 0 10px !important;
+          }
+        }
+
+        span {
+          cursor: pointer;
+          padding: 0 30px;
+          display: inline-block;
+          font-size: 12px;
+          height: 60px;
+          line-height: 60px;
+        }
+
+        .active-tab {
           color: #fff;
-          font-size: 14px;
-          height: 45px;
-          line-height: 45px;
-          font-weight: bold;
+          border-bottom: 1px solid #fff;
         }
       }
-      thead {
+
+      .order-table {
+        border-collapse: collapse;
+      }
+
+      table {
+        vertical-align: center;
+
         tr {
-          border-bottom: none !important;
+          border-top: 1px dashed #1e1e1e;
+          background: #1c233f;
+
+          th,
+          td {
+            color: #fff;
+            font-size: 14px;
+            height: 45px;
+            line-height: 45px;
+            font-weight: bold;
+          }
+        }
+
+        thead {
+          tr {
+            border-bottom: none !important;
+          }
         }
       }
-    }
-    .tab3-class {
-      background: #282828;
-      height: 45px;
-      line-height: 45px;
+
+      .tab3-class {
+        background: #282828;
+        height: 45px;
+        line-height: 45px;
+      }
     }
   }
-}
-.win {
-  background-color: #2a402a !important;
-  border-color: #2d742d !important;
-}
-.lose {
-  background-color: #3e2626 !important;
-  border-color: #7a3022 !important;
-}
+
+  .win {
+    background-color: #2a402a !important;
+    border-color: #2d742d !important;
+  }
+
+  .lose {
+    background-color: #3e2626 !important;
+    border-color: #7a3022 !important;
+  }
 </style>
--- src/network/transtion.js
+++ src/network/transtion.js
@@ -3,14 +3,14 @@ import Eos from "eosjs";
 
 export const network = {
   blockchain: "eos",
-  host: "hapi.bos.eosrio.io",
+  host: "bos.caleos.io",
   port: 443,
   protocol: "https",
   chainId: "d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86"
 }
 
 export const restApi = Eos({
-  httpEndpoint: `${network.protocol}://api.bossweden.org:${network.port}`,
+  httpEndpoint: `${network.protocol}://api.bos.eosrio.io:${network.port}`,
   chainId: network.chainId
 })
 
